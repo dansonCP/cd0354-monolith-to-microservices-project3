fa6fc0d9e3fa79dc3b2c1d4bd023ff1b28a7628f
Danson Waweru <DWaweru@us.ci.org>
2022-11-17 20:35:49 +0300

    Added external IPs to reverseproxy code base. Updated .travis.yaml file to change tag for frontend to have 4a

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a14a59add1ec54447be3d3a7dc37fa0e-1927218996.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
